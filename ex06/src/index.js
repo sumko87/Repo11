8c716ebaf8affc68b0180597107063f1a2b8dd8f
Sumedin Bilalic <sumko87@gmail.com>
2022-04-08 00:44:34 +0200

    small changes in ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -4,5 +4,5 @@ function myNestedFunction(arr) {
     return myNestedArray;
 }
 // Only change code above this line
-console.log(myNestedFunction()); // Change this line
+console.log(myNestedFunction(myNestedArray)); // Change this line
 module.exports = myNestedFunction;
