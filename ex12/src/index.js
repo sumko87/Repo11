ae434ba4f30dec3b28d870ff3610ebc1e1d81cb6
Sumedin Bilalic <sumko87@gmail.com>
2022-04-08 04:24:28 +0200

    splitting arrays into chunks

---
+++ ex12/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function splitArrayInGroups(arr,n) {
+    var result=[];
+    for (let i = 0; i<arr.length; i+=n) {
+        const chunk = arr.slice(i, i + n);
+        result.push(chunk);
+    }
+    return result;
+}
+// Only change code above this line
+console.log(splitArrayInGroups(["a", "b", "c", "d"], 2)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3)); // Change this line
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)); // Change this line
+module.exports = splitArrayInGroups;
