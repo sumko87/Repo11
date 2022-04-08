93c526c295329257adea9ee1abab5b4d1e2bcba5
Sumedin Bilalic <sumko87@gmail.com>
2022-04-08 10:38:59 +0200

    fixing variable naming in ex02

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,10 +1,10 @@
 // Only change code below this code
-var myString ="Paragon is the best!";
+var myStr ="Paragon is the best!";
 function useMethods(str) {
     var myReverse=str.split("").reverse().join("");
     var countCharacters=myReverse.split(" ").map(x=>x.length);
     return {myReverse, countCharacters};
 }
-console.log(useMethods(myString));
+console.log(useMethods(myStr));
 // Only change code above this code
 module.exports = useMethods;
